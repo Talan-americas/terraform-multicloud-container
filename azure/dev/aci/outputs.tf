9ba63b4658afee4736b374d8d7f4618fb18231ca
Carlos Zela <c.zelabueno@gmail>
2023-04-25 23:33:05 +0000

    clean files removed

--- azure/dev/aci/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "azure_container_instance_resource_group_name" {
-  value = azurerm_container_group.acigroup.resource_group_name
-}
-
-output "azure_container_instance_id" {
-  value = azurerm_container_group.acigroup.id
-}
